6981d0e0bb160ab77fc92573c052a2ca48ca41bb
Fridah-kalee <fridahkalee@gmail.com>
2022-06-11 12:33:01 +0300

    create admin page

--- reviews/models.py
+++ reviews/models.py
@@ -5,4 +5,10 @@ class Profile(models.Model):
     username = models.CharField(max_length=30)
     bio = models.TextField()
     profile_pic = models.ImageField(upload_to='profile/',blank=True,default='')
-    date_created =models.DateField(auto_now_add=True) 
+    date_created =models.DateField(auto_now_add=True)
+    
+    def save_profile(self):
+        self.save
+    
+    def __str__(self):
+        return self.username 
