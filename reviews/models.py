46de24aea7ecf69215119d480a425567f26748ec
Fridah-kalee <fridahkalee@gmail.com>
2022-06-11 13:21:04 +0300

    create search form

--- reviews/models.py
+++ reviews/models.py
@@ -12,3 +12,20 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.username 
+
+class Project(models.Model):
+    title = models.TextField(max_length=30)
+    link= models.URLField(max_length=200)
+    description = models.TextField(max_length=300)
+    date_created= models.DateField(auto_now_add=True )
+
+    def save_project(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects    
