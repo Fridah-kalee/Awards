b66782a4cb1fe87ead8cee674348f977e8f978bd
Fridah-kalee <fridahkalee@gmail.com>
2022-06-11 10:39:25 +0300

    config SECRET KEY

--- awards/settings.py
+++ awards/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gn-oze)to9fjrqr0zo8=@89p7*4j7o)_pfc)mnuh1e&dl52#u'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
