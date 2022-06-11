07f9a65870e29d0a300eeb829a7be3bb47521174
Fridah-kalee <fridahkalee@gmail.com>
2022-06-11 11:26:36 +0300

    connecting view function and URL

--- awards/settings.py
+++ awards/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-!gn-oze)to9fjrqr0zo8=@89p7*4j7o)_pfc)mnuh1e&dl52#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
